e8e5ae8e5acd3d380bb62a5a158dafa4b7315923
Mohamed KABECHE <mohamed.kabeche@reseau.sncf.fr>
2026-05-26 11:17:58 +0200

    correction complète de salleDeSport

--- src/main/java/com/tp/sport/SalleDeSport.java
+++ src/main/java/com/tp/sport/SalleDeSport.java
@@ -1,24 +1,65 @@
 package com.tp.sport;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class SalleDeSport{
 
-public SalleDeSport() {
-    this.adherents = new ArrayList<>();
-    this.seances   = new ArrayList<>();
-}
+public class SalleDeSport {
+    private List<Adherent> adherents;
+    private List<Seance> seances;
 
-public Adherent trouverAdherent(int id) {
-    for (Adherent a : adherents) {
-        if (a.getId() == id) return a;
+    //Constructeur
+    public SalleDeSport () {
+        this.adherents = new ArrayList<>();
+        this.seances = new ArrayList<>();
     }
-    throw new RuntimeException("Adhérent introuvable par id=" + id);
-}
+    //Getters
+    public List<Adherent> getAdherents() {
+        return adherents;
+    }
+    public List<Seance> getSeances() {
+        return seances;
+    }
 
-@Override
-public String toString() {
-    return "SalleDeSport{nbAdherents=" + adherents.size() + ", nbSeances=" + seances.size() + "}";
-}
+    //Méthodes
+    public void ajouterAdherent(Adherent a) {
+        if (a !=null) {
+            adherents.add(a);
+        }
+    }
+    public void ajouterSeance(Seance s) {
+        if (s !=null) {
+            seances.add(s);
+        }
+    }
+    public List<Seance> seancesDisponibles() {
+        return seances;
+    }
+    public List<Adherent> adherentsAvecSauna() {
+        List<Adherent> resultat = new ArrayList<>();
+        for (Adherent a : adherents) {
+            if (a.getAbonnement() !=null && a.getAbonnement() .permetAccesSauna()) {
+                resultat.add(a);
+            }
+        }
+        return resultat;
+    }
+    public Adherent trouverAdherent(int id) {
+        for (Adherent a : adherents) {
+            if (a.getId() ==id) {
+                return a;
+            }
+        }
+        throw new RuntimeException("Adhérent introuvable par id=:" + id);
+
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "Salle de sport" + "nombre d'adhérents:" + adherents.size() + "nombre de séances:" + seances.size();
+    }
+
 
 }
