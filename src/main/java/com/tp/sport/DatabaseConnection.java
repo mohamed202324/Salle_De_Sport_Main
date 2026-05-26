76029d9b431d485541bc631f9857e1f41aa187d6
Mohamed KABECHE <mohamed.kabeche@reseau.sncf.fr>
2026-05-26 15:05:19 +0200

    condition logger

--- src/main/java/com/tp/sport/DatabaseConnection.java
+++ src/main/java/com/tp/sport/DatabaseConnection.java
@@ -1,15 +1,15 @@
 package com.tp.sport;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL="jdbc:mysql//localhost:8889/salle_de_sport";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
 
+    private static final String URL  = "jdbc:mysql://localhost:8889/salle_de_sport";
+    private static final String USER = "root";
+    // Lit le mot de passe depuis une variable d'environnement
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     private DatabaseConnection() {
         throw new UnsupportedOperationException("Classe utilitaire, non instanciable");
@@ -18,10 +18,9 @@ public class DatabaseConnection {
     public static Connection getConnection() throws SQLException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(URL, USER,PASSWORD);
+            return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (ClassNotFoundException e) {
             throw new SQLException("MySql Driver non trouvé", e);
         }
-        
-    } 
+    }
 }
--- src/main/java/com/tp/sport/Main.java
+++ src/main/java/com/tp/sport/Main.java
@@ -1,12 +1,12 @@
 package com.tp.sport;
 
 import java.time.LocalDateTime;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
 
     private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
-
     public static void main(String[] args) {
 
         Prestation sauna = new Prestation("Sauna", "Accès sauna", 5.80);
@@ -14,19 +14,20 @@ public class Main {
 
         Seance bodyPump = new Seance(1, "BodyPump",
                                      LocalDateTime.of(2026, 3, 10, 18, 0), 10);
-        Seance yoga = new Seance(2, "Yoga",
-                                 LocalDateTime.of(2026, 3, 17, 19, 0), 20);
+
 
         Reservation r1 = new Reservation(bodyPump);
         r1.ajouterPrestation(sauna);
         r1.ajouterPrestation(coach);
 
-        LOGGER.info("Réservation :");
-        LOGGER.info(r1.toString());
-        LOGGER.info("Cout prestations = " + r1.coutPrestation() + " euros");
+        if (!r1.getPrestations().isEmpty()) {
+            LOGGER.log(Level.INFO, "Réservation : {0}", r1);
+            LOGGER.log(Level.INFO, "Cout prestations = {0} euros", r1.coutPrestation());
+        }
 
         r1.annuler();
-        LOGGER.info("Après annulation :");
-        LOGGER.info("Statut = " + r1.getStatut());
+        LOGGER.log(Level.INFO, "Après annulation - Statut = {0}", r1.getStatut());
+
+
     }
 }
